8a67ef2976763e13d35c3e499c0602dbef43881f
marcdjbn <marcdjabian@gmail.com>
2025-09-04 12:21:08 -0500

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,1 +1,19 @@
+CREATE DATABASE rc_db;
+USE rc_db;
+
+CREATE TABLE products(
+  productID int PRIMARY KEY,
+  productName VARCHAR(50),
+  numInStock int,
+  price DECIMAL(10,2)
+);
+
+INSERT INTO products(productID, productName, numInStock, price) VALUES 
+(1, 'Jeans', 45, 59.99),
+(2, 'Khakis', 67, 30.99),
+(3, 'T-shirt', 23, 19.99),
+(4, 'Hoodie', 44, 25.99),
+(5, 'Shoes', 56, 80.99);
+
+SELECT * FROM products;
 
