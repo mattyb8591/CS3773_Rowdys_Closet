c0b1f64836af69b37472fc29d4aeb0389b7c9da2
marcdjbn <marcdjabian@gmail.com>
2025-10-02 10:17:18 -0500

    rename functs

--- app.py
+++ app.py
@@ -27,9 +27,13 @@ def get_db_connection():
 def index():
     return render_template('index.html')
 
-@app.route('/signin')
-def signin():
-    return render_template('signin.html')
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 @app.route('/home')
 def home():
