20b4577e371aa34cae9abd58ff6282aa48c1e29e
marcdjbn <marcdjabian@gmail.com>
2025-10-02 10:27:15 -0500

    account sql table

--- schema.sql
+++ schema.sql
@@ -8,9 +8,17 @@ CREATE TABLE products(
   rating DEcIMAL(2,1)
 );
 
+CREATE TABLE accounts(
+  accountID int AUTO_INCREMENT PRIMARY KEY,
+  userName VARCHAR(50),
+  passwordHash VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
 INSERT INTO products(productID, productName, numInStock, price, rating) VALUES 
 (1, 'Jeans', 45, 59.99, 4.5),
 (2, 'Khakis', 67, 30.99, 1.1),
 (3, 'T-shirt', 23, 19.99, 3.9),
 (4, 'Hoodie', 44, 25.99, 5.0),
 (5, 'Shoes', 56, 80.99, 4.8);
+
