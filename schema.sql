a316987924227eb2303cdded10384aa163ece2a2
mattyb8591 <bryanrobillard29@yahoo.com>
2025-10-21 10:27:16 -0500

    added schema

--- schema.sql
+++ schema.sql
@@ -1,25 +1,83 @@
+
 USE rowdys_closet_db;
 
-CREATE TABLE products(
-  productID int PRIMARY KEY,
-  productName VARCHAR(50),
-  numInStock int,
-  price DECIMAL(10,2),
-  rating DEcIMAL(2,1)
+CREATE TABLE IF NOT EXISTS addresses(
+  address_id INT PRIMARY KEY AUTO_INCREMENT,
+  city VARCHAR(50) NOT NULL,
+  state_abrev VARCHAR(2) NOT NULL,
+  zip_code INT NOT NULL
 );
 
-CREATE TABLE accounts(
-  accountID int AUTO_INCREMENT PRIMARY KEY,
-  userName VARCHAR(50),
-  email VARCHAR(50) UNIQUE,
-  passwordHash VARCHAR(255) NOT NULL,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE IF NOT EXISTS users(
+  user_id INT PRIMARY KEY AUTO_INCREMENT,
+  username VARCHAR(50) NOT NULL,
+  password VARCHAR(50) NOT NULL,
+  email VARCHAR(25) NOT NULL,
+  phone_number VARCHAR(20),
+  address_id INT,
+  FOREIGN KEY (address_id) references addresses(address_id)
 );
 
-INSERT INTO products(productID, productName, numInStock, price, rating) VALUES 
-(1, 'Jeans', 45, 59.99, 4.5),
-(2, 'Khakis', 67, 30.99, 1.1),
-(3, 'T-shirt', 23, 19.99, 3.9),
-(4, 'Hoodie', 44, 25.99, 5.0),
-(5, 'Shoes', 56, 80.99, 4.8);
+CREATE TABLE IF NOT EXISTS cart_products(
+  cart_product_id INT PRIMARY KEY AUTO_INCREMENT,
+  cart_id INT NOT NULL,
+  product_id INT NOT NULL,
+  FOREIGN KEY (cart_id) REFERENCES carts(cart_id),
+  FOREIGN KEY (product_id) REFERENCES products(product_id)
+);
+
+CREATE TABLE IF NOT EXISTS customers(
+  customer_id INT PRIMARY KEY AUTO_INCREMENT,
+  user_id INT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE IF NOT EXISTS admins(
+  admin_id INT PRIMARY KEY AUTO_INCREMENT,
+  user_id INT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE IF NOT EXISTS products(
+  product_id INT PRIMARY KEY AUTO_INCREMENT,
+  price DECIMAL(10,2) NOT NULL,
+  size  VARCHAR(5) NOT NULL,
+  name VARCHAR(50) NOT NULL,
+  stock INT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS orders(
+  order_id INT PRIMARY KEY AUTO_INCREMENT,
+  total DECIMAL(10,2) NOT NULL,
+  discount_code VARCHAR(50),
+  order_status VARCHAR(20) NOT NULL,
+  order_date DATE NOT NULL,
+  customer_id INT NOT NULL,
+  cart_product_id INT NOT NULL,
+  payment_id INT NOT NULL,
+  FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
+  FOREIGN KEY (cart_product_id) REFERENCES cart_products(cart_product_id),
+  FOREIGN KEY (payment_id) REFERENCES payments(payment_id)
+);
+
+CREATE TABLE IF NOT EXISTS payments(
+  payment_id INT PRIMARY KEY AUTO_INCREMENT,
+  payment_type VARCHAR(20) NOT NULL,
+  details VARCHAR(200) NOT NULL,
+  status VARCHAR(20) NOT NULL,
+  customer_id INT NOT NULL,
+  FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+);
+
+CREATE TABLE IF NOT EXISTS carts(
+  cart_id INT PRIMARY KEY AUTO_INCREMENT,
+  customer_id INT NOT NULL,
+  FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+);
+
+
+
+
+
+
 
