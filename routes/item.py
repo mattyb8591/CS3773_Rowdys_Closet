8b96b6e0f1f804cedf62f56a3b0e6fce6099fd56
DJ R <manyuuisyourmaster@gmail.com>
2025-10-30 10:35:34 -0500

    item file(may need to be changed)

--- routes/item.py
+++ routes/item.py
@@ -5,6 +5,34 @@ from werkzeug.security import check_password_hash
 item_bp = Blueprint("item", __name__, template_folder="templates", static_folder="static")
 
 @item_bp.route("/", methods=["GET"])
-def index():
+def index(item_id):
+    #db connection
+
+    db = current_app.get_db_connection()
+
+    cursor = db.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM products where product_id =%s", (item_id))
+    item_data = cursor.fetchone()
+    #get item data
+    for row in item_data:
+        name = row['name']
+        price = row['price']
+        size = row['size']
+        stock = row['stock']
     
-    return render_template("item.html")
+    if request.method == "GET":
+        return render_template("item.html", item_name=name, price=price,size=size, stock=stock)
+
+    if request.method == "POST":
+        quantity = request.method['quantity']
+        if quantity >= stock:
+            return 'out of stock'
+        
+        stock = stock - quantity
+        cursor.execute("UPDATE products SET stock = %s WHERE product_id = %s", (stock, item_id))
+        db.commit()
+    cursor.close()
+    db.close()
+    
+    return render_template("item.html", item_name=name, price=price,size=size, stock=stock)
+    
