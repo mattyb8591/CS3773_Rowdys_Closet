45918543692b1fe7523f4a4bb1dd1720effac575
marcdjbn <marcdjabian@gmail.com>
2025-11-04 21:04:05 -0600

    fix frontend issues

--- routes/profile.py
+++ routes/profile.py
@@ -67,44 +67,57 @@ def update():
     cursor = db.cursor(dictionary=True)
     
     try:
-
         cursor.execute("SELECT * FROM users WHERE user_id = %s", (user_id,))
         user = cursor.fetchone()
         
         address_id = user.get("address_id")
-
+        
         if any(key in data for key in ['street_number', 'street_name', 'city', 'state', 'zip_code']):
             if address_id:
-
+                cursor.execute("SELECT * FROM addresses WHERE address_id = %s", (address_id,))
+                current_address = cursor.fetchone()
+                
+                street_number = data.get('street_number') or current_address['street_number']
+                street_name = data.get('street_name') or current_address['street_name']
+                city = data.get('city') or current_address['city']
+                state = data.get('state') or current_address['state_abrev']
+                zip_code = data.get('zip_code') or current_address['zip_code']
+                
                 cursor.execute("""
                     UPDATE addresses 
                     SET street_number = %s, street_name = %s, city = %s, state_abrev = %s, zip_code = %s
                     WHERE address_id = %s
                 """, (
-                    data.get('street_number'),
-                    data.get('street_name'), 
-                    data.get('city'),
-                    data.get('state'),
-                    data.get('zip_code'),
+                    street_number,
+                    street_name, 
+                    city,
+                    state,
+                    zip_code,
                     address_id
                 ))
             else:
-
-                cursor.execute("""
-                    INSERT INTO addresses (street_number, street_name, city, state_abrev, zip_code)
-                    VALUES (%s, %s, %s, %s, %s)
-                """, (
-                    data.get('street_number'),
-                    data.get('street_name'),
-                    data.get('city'),
-                    data.get('state'),
-                    data.get('zip_code')
-                ))
-                address_id = cursor.lastrowid
+                street_number = data.get('street_number') or ''
+                street_name = data.get('street_name') or ''
+                city = data.get('city') or ''
+                state = data.get('state') or ''
+                zip_code = data.get('zip_code') or ''
                 
-                cursor.execute("""
-                    UPDATE users SET address_id = %s WHERE user_id = %s
-                """, (address_id, user_id))
+                if any([street_number, street_name, city, state, zip_code]):
+                    cursor.execute("""
+                        INSERT INTO addresses (street_number, street_name, city, state_abrev, zip_code)
+                        VALUES (%s, %s, %s, %s, %s)
+                    """, (
+                        street_number,
+                        street_name,
+                        city,
+                        state,
+                        zip_code
+                    ))
+                    address_id = cursor.lastrowid
+                    
+                    cursor.execute("""
+                        UPDATE users SET address_id = %s WHERE user_id = %s
+                    """, (address_id, user_id))
         
         update_fields = []
         update_values = []
