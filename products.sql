4d3c2bb6e67065c2977922f2e38a1b483591532e
mattyb8591 <bryanrobillard29@yahoo.com>
2025-11-05 23:20:46 -0600

    products.sql changes

--- products.sql
+++ products.sql
@@ -1,5 +1,102 @@
 USE rowdys_closet_db;
 
 
--- Insert all products
+INSERT VALUES (price, size, name, stock, description, type, img_file_path)
+VALUES
+(42.00, "Small", "UTSA Hoodie", 25, "N/A", "Hoodies", "/static/img/product_images/"),
+(42.00, "Small", "UTSA Roadrunnner Head Hoodie", 35, "N/A", "Hoodies", "/static/img/product_images/"),
+(42.00, "Small", "UTSA Football Verticle Hoodie", 25, "N/A", "Hoodies", "/static/img/product_images/"),
+(42.00, "Small", "UTSA Hoodie", 25, "N/A", "Hoodies", "/static/img/product_images/"),
+(42.00, "Small", "UTSA Ugly Christmas Sweater", 25, "N/A", "Hoodies", "/static/img/product_images/"),
+(42.00, "Small", "UTSA Roadrunner Head - Full Zip Hoodie", 25, "N/A", "Hoodies", "/static/img/product_images/"),
+(57.00, "Small", "UTSA Full Mascot Vintage Sweatshirt", 25, "N/A", "Hoodies", "/static/img/product_images/"),
+(57.00, "Small", "UTSA Script Vintage", 25, "N/A", "Hoodies", "/static/img/product_images/"),
+(58.00, "Small", "UTSA Softshell Core Vest", 25, "N/A", "Jackets", "/static/img/product_images/"),
+(68.00, "Small", "UTSA Black Charger Jacket", 25, "N/A", "Jackets", "/static/img/product_images/"),
+(64.00, "Small", "UTSA Black Softshell Jacket", 25, "N/A", "Jackets", "/static/img/product_images/"),
+(155.00, "Small", "UTSA Black Heather Jacket", 25, "N/A", "Jackets", "/static/img/product_images/"),
+(310.00, "Small", "UTSA Black Thermoball Trekker Jacket ", 15, "N/A", "Jackets", "/static/img/product_images/"),
+(230.00, "Small", "UTSA Black Apex Barrier Softwhell Jacket", 25, "N/A", "Jackets", "/static/img/product_images/"),
+(57.00, "Small", "UTSA Sport Tek Sport Wick Stretch Jacket", 25, "N/A", "Jackets", "/static/img/product_images/"),
+(170.00, "Small", "UTSA Black Vortex Waterproof 3 in 1 Jacket", 25, "N/A", "Jackets", "/static/img/product_images/"),
+(22.00, "Small", "UTSA Roadrunner Head T-Shirt", 25, "N/A", "T-Shirts", "/static/img/product_images/"),
+(22.00, "Small", "UTSA Logo T-Shirt", 25, "N/A", "T-Shirts", "/static/img/product_images/"),
+(22.00, "Small", "UTSA Roadrunner Arched T-Shirt", 25, "N/A", "T-Shirts", "/static/img/product_images/"),
+(22.00, "Small", "UTSA Ugly Christmas T-Shirt", 25, "N/A", "T-Shirts", "/static/img/product_images/"),
+(22.00, "Small", "UTSA Women's Basketball Runners T-Shirt", 25, "N/A", "T-Shirts", "/static/img/product_images/"),
+(22.00, "Small", "UTSA Fifteenth T-Shirt", 25, "N/A", "T-Shirts", "/static/img/product_images/"),
+(22.00, "Small", "UTSA Baseball T-Shirt", 25, "N/A", "T-Shirts", "/static/img/product_images/"),
+(22.00, "Small", "UTSA Performance Tee", 25, "N/A", "T-Shirts", "/static/img/product_images/"),
+(42.00, "Medium", "UTSA Hoodie", 25, "N/A", "Hoodies", "/static/img/product_images/"),
+(42.00, "Medium", "UTSA Roadrunnner Head Hoodie", 35, "N/A", "Hoodies", "/static/img/product_images/"),
+(42.00, "Medium", "UTSA Football Verticle Hoodie", 25, "N/A", "Hoodies", "/static/img/product_images/"),
+(42.00, "Medium", "UTSA Hoodie", 25, "N/A", "Hoodies", "/static/img/product_images/"),
+(42.00, "Medium", "UTSA Ugly Christmas Sweater", 25, "N/A", "Hoodies", "/static/img/product_images/"),
+(42.00, "Medium", "UTSA Roadrunner Head - Full Zip Hoodie", 25, "N/A", "Hoodies", "/static/img/product_images/"),
+(57.00, "Medium", "UTSA Full Mascot Vintage Sweatshirt", 25, "N/A", "Hoodies", "/static/img/product_images/"),
+(57.00, "Medium", "UTSA Script Vintage", 25, "N/A", "Hoodies", "/static/img/product_images/"),
+(58.00, "Medium", "UTSA Softshell Core Vest", 25, "N/A", "Jackets", "/static/img/product_images/"),
+(68.00, "Medium", "UTSA Black Charger Jacket", 25, "N/A", "Jackets", "/static/img/product_images/"),
+(64.00, "Medium", "UTSA Black Softshell Jacket", 25, "N/A", "Jackets", "/static/img/product_images/"),
+(155.00, "Medium", "UTSA Black Heather Jacket", 25, "N/A", "Jackets", "/static/img/product_images/"),
+(310.00, "Medium", "UTSA Black Thermoball Trekker Jacket ", 15, "N/A", "Jackets", "/static/img/product_images/"),
+(230.00, "Medium", "UTSA Black Apex Barrier Softwhell Jacket", 25, "N/A", "Jackets", "/static/img/product_images/"),
+(57.00, "Medium", "UTSA Sport Tek Sport Wick Stretch Jacket", 25, "N/A", "Jackets", "/static/img/product_images/"),
+(170.00, "Medium", "UTSA Black Vortex Waterproof 3 in 1 Jacket", 25, "N/A", "Jackets", "/static/img/product_images/"),
+(22.00, "Medium", "UTSA Roadrunner Head T-Shirt", 25, "N/A", "T-Shirts", "/static/img/product_images/"),
+(22.00, "Medium", "UTSA Logo T-Shirt", 25, "N/A", "T-Shirts", "/static/img/product_images/"),
+(22.00, "Medium", "UTSA Roadrunner Arched T-Shirt", 25, "N/A", "T-Shirts", "/static/img/product_images/"),
+(22.00, "Medium", "UTSA Ugly Christmas T-Shirt", 25, "N/A", "T-Shirts", "/static/img/product_images/"),
+(22.00, "Medium", "UTSA Women's Basketball Runners T-Shirt", 25, "N/A", "T-Shirts", "/static/img/product_images/"),
+(22.00, "Medium", "UTSA Fifteenth T-Shirt", 25, "N/A", "T-Shirts", "/static/img/product_images/"),
+(22.00, "Medium", "UTSA Baseball T-Shirt", 25, "N/A", "T-Shirts", "/static/img/product_images/"),
+(22.00, "Medium", "UTSA Performance Tee", 25, "N/A", "T-Shirts", "/static/img/product_imagLarge"
+(42.00, "Large", "UTSA Roadrunnner Head Hoodie", 35, "N/A", "Hoodies", "/static/img/product_images/"),
+(42.00, "Large", "UTSA Hoodie", 25, "N/A", "Hoodies", "/static/img/product_images/"),
+(42.00, "Large", "UTSA Football Verticle Hoodie", 25, "N/A", "Hoodies", "/static/img/product_images/"),
+(42.00, "Large", "UTSA Hoodie", 25, "N/A", "Hoodies", "/static/img/product_images/"),
+(42.00, "Large", "UTSA Ugly Christmas Sweater", 25, "N/A", "Hoodies", "/static/img/product_images/"),
+(42.00, "Large", "UTSA Roadrunner Head - Full Zip Hoodie", 25, "N/A", "Hoodies", "/static/img/product_images/"),
+(57.00, "Large", "UTSA Full Mascot Vintage Sweatshirt", 25, "N/A", "Hoodies", "/static/img/product_images/"),
+(57.00, "Large", "UTSA Script Vintage", 25, "N/A", "Hoodies", "/static/img/product_images/"),
+(58.00, "Large", "UTSA Softshell Core Vest", 25, "N/A", "Jackets", "/static/img/product_images/"),
+(68.00, "Large", "UTSA Black Charger Jacket", 25, "N/A", "Jackets", "/static/img/product_images/"),
+(64.00, "Large", "UTSA Black Softshell Jacket", 25, "N/A", "Jackets", "/static/img/product_images/"),
+(155.00, "Large", "UTSA Black Heather Jacket", 25, "N/A", "Jackets", "/static/img/product_images/"),
+(310.00, "Large", "UTSA Black Thermoball Trekker Jacket ", 15, "N/A", "Jackets", "/static/img/product_images/"),
+(230.00, "Large", "UTSA Black Apex Barrier Softwhell Jacket", 25, "N/A", "Jackets", "/static/img/product_images/"),
+(57.00, "Large", "UTSA Sport Tek Sport Wick Stretch Jacket", 25, "N/A", "Jackets", "/static/img/product_images/"),
+(170.00, "Large", "UTSA Black Vortex Waterproof 3 in 1 Jacket", 25, "N/A", "Jackets", "/static/img/product_images/"),
+(22.00, "Large", "UTSA Roadrunner Head T-Shirt", 25, "N/A", "T-Shirts", "/static/img/product_images/"),
+(22.00, "Large", "UTSA Logo T-Shirt", 25, "N/A", "T-Shirts", "/static/img/product_images/"),
+(22.00, "Large", "UTSA Roadrunner Arched T-Shirt", 25, "N/A", "T-Shirts", "/static/img/product_images/"),
+(22.00, "Large", "UTSA Ugly Christmas T-Shirt", 25, "N/A", "T-Shirts", "/static/img/product_images/"),
+(22.00, "Large", "UTSA Women's Basketball Runners T-Shirt", 25, "N/A", "T-Shirts", "/static/img/product_images/"),
+(22.00, "Large", "UTSA Fifteenth T-Shirt", 25, "N/A", "T-Shirts", "/static/img/product_images/")
+(22.00, "Large", "UTSA Baseball T-Shirt", 25, "N/A", "T-Shirts", "/static/img/product_images/"),
+(22.00, "Large", "UTSA Performance Tee", 25, "N/A", "T-Shirts", "/static/img/product_images/"),
+(18.00, NULL, "Brushed Bull Denim Visor 210", 25, "N/A", "Headwear", "/static/img/product_images/"),
+(21.00, NULL, "Structured Adjustable Pro Style Hat - Mascot", 25, "N/A", "Headwear", "/static/img/product_images/");
+(21.00, NULL, "Mountain Beanie - UTSA Roadrunner Head - Institutional Mark", 25, "N/A", "Headwear", "/static/img/product_images/"),
+(32.00, NULL, "Mountain Beanie - UTSA Roadrunner Head", 25, "N/A", "Headwear", "/static/img/product_images/"),
+(45.00, NULL, "Knit Beanie w/Cuff - UTSA Roadrunner Head", 25, "N/A", "Headwear", "/static/img/product_images/"),
+(15.00, NULL, "Sportsman Pom Pom Cuffed Beanie", 25, "N/A", "Headwear", "/static/img/product_images/"),
+(82.00, NULL, "Black Roam Lite Backpack", 25, "N/A", "Bags", "/static/img/product_images/");
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- Insert all products
